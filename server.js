e48b2f79ad75a0d02bc28bdac46149825582237e
Destroyerofdarkness <dawidadogier@gmail.com>
2025-10-24 15:25:06 +0200

    Made a styling for general layout and added historie.ejs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 const ejs = require("ejs")
+const path = require("path")
 
+app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({urlencoded:true}))
 app.use(express.json())
 app.set("view engine", "ejs")
@@ -9,6 +11,8 @@ app.set("view engine", "ejs")
 app.get("/", (req,res)=>{
     res.render("index")
 })
-
+app.get("/historie", (req,res)=>{
+    res.render("historie")
+})
 
 app.listen(3000)
